3a933b61229fb1476ec4a458ef3f9d7b1df8c8d9
W-white <62198612+W-white@users.noreply.github.com>
2020-03-15 16:06:21 +0900

    Add files via upload

---
+++ lesson/corect.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>漢字探し</title>
+        
+        <style>
+            
+        
+            body {
+                background-color:rgb(181,241,153);
+            }
+            h1 {
+                text-align:center;
+                margin-top:100px;
+            }
+            a{
+                display:inline-block;
+                text-align:center;
+                font-size:2em;
+                margin:0px;
+                text-decoration : none;
+                color:inherit;
+            }
+            p{
+                display:inline-block;
+                text-align:center;
+                font-size:2em;
+                margin:0px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>正解です！！</h1>
+        
+    </body>
+</html>
